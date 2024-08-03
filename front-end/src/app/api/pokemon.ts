d0fc6d5a0b9dbb752e1e54917ac2f28cbcf16c06
Lucas Picone <luag1998@gmail.com>
2024-08-03 00:44:30 -0300

    backend call and basic layout

---
+++ front-end/src/app/api/pokemon.ts
@@ -0,0 +1,49 @@
+import axios from "axios"
+
+export enum PokemonType {
+    NORMAL = 'NORMAL',
+    FIRE = 'FIRE',
+    WATER = 'WATER',
+    ELECTRIC = 'ELECTRIC',
+    GRASS = 'GRASS',
+    ICE = 'ICE',
+    FIGHTING = 'FIGHTING',
+    POISON = 'POISON',
+    GROUND = 'GROUND',
+    FLYING = 'FLYING',
+    PSYCHIC = 'PSYCHIC',
+    BUG = 'BUG',
+    ROCK = 'ROCK',
+    GHOST = 'GHOST',
+    DRAGON = 'DRAGON',
+    DARK = 'DARK',
+    STEEL = 'STEEL',
+    FAIRY = 'FAIRY',
+}
+
+export interface Pokemon {
+    id: string
+    name: string,
+    attack: number,
+    defense: number,
+    hp: number,
+    speed: number,
+    type: PokemonType,
+    imageUrl: string
+}
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8080/api'
+})
+
+const fetchPokemon = async () => {
+    try {
+        const { data }: { data: Pokemon[]} = await axiosInstance('/pokemon');
+        return data
+    } catch (error) {
+        console.error('Error on Backend:', error)
+        throw new Error('Could not get data')
+    }
+}
+
+export default fetchPokemon
